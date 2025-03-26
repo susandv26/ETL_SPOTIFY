bc3c07bcd4d1b0ca05c4a01587fdccbddcd67f6f
susandv26 <valladares.susan@yahoo.com>
2025-03-26 00:45:22 -0600

    conexion a la base de datos

--- ETL.py
+++ ETL.py
@@ -1,6 +1,4 @@
 
-#print('Hola mundo');
-
 import pyodbc
 
 # Configuración de la conexión origen
@@ -10,20 +8,32 @@ usuarioOrigen = 'sa'
 passwordOrigen = '123'
 
 # Cadena de conexión
-conexionOrigen = pyodbc.connect(f'DRIVER={{SQL Server}};'
-                        f'SERVER={serverOrigen};'
-                        f'DATABASE={databaseOrigen};'
-                        f'UID={usuarioOrigen};'
-                        f'PWD={passwordOrigen}')
+try:
+    print("Conectando a la base de datos...")
+    # Cadena de conexión
+    conexionOrigen = pyodbc.connect(f'DRIVER={{SQL Server}};'
+                                    f'SERVER={serverOrigen};'
+                                    f'DATABASE={databaseOrigen};'
+                                    f'UID={usuarioOrigen};'
+                                    f'PWD={passwordOrigen}')
+    print("Conexión exitosa")
 
-# Crear cursor y ejecutar consulta origen
-cursor = conexionOrigen.cursor()
-cursor.execute("SELECT * FROM dbo.Artistas")
+    # Crear cursor y ejecutar consulta origen
+    cursor = conexionOrigen.cursor()
+    cursor.execute("SELECT * FROM dbo.Artistas")
 
-# Obtener y mostrar los datos origen
-for fila in cursor.fetchall():
-    print(fila)
+    # Obtener y mostrar los datos origen
+    filas = cursor.fetchall()
+    if filas:
+        for fila in filas:
+            print(fila)
+    else:
+        print("No se encontraron registros.")
 
-# Cerrar la conexión
-cursor.close()
-conexionOrigen.close()
+    # Cerrar la conexión
+    cursor.close()
+    conexionOrigen.close()
+
+except Exception as e:
+    print("Error al conectar a SQL Server:")
+    print(e)
